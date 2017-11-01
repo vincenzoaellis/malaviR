8a6c3fef2b63969ebe1152c962f5df85574e609e
Vincenzo A Ellis <vincenzoaellis@gmail.com>
2017-11-01 09:01:09 +0100

    added a function (malavi_version) to extract the current version number and latest update date from MalAvi

---
+++ R/malavi_version.R
@@ -0,0 +1,12 @@
+#' Get version number and upload date from MalAvi website
+#' @importFrom magrittr %>%
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_text
+#' @importFrom stringr str_extract
+#' @export
+
+malavi_version <- function(){
+  read_html("http://mbio-serv2.mbioekol.lu.se/Malavi/JavaScript/footer.js") %>%
+    html_text() %>%
+    str_extract(pattern = "Version [0-9].[0-9].[0-9], [A-Z][a-z]+ [0-9]:[a-z]+, [0-9]+")
+}
