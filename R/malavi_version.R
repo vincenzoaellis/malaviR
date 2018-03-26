8933be9aeb1d9af79ff1b936056097eee3a7d653
Vincenzo A Ellis <vincenzoaellis@gmail.com>
2018-03-26 14:53:07 +0200

    update to malavi_version to reflect new way that the upload date is reported on the malavi homepage

--- R/malavi_version.R
+++ R/malavi_version.R
@@ -8,5 +8,5 @@
 malavi_version <- function(){
   read_html("http://mbio-serv2.mbioekol.lu.se/Malavi/JavaScript/footer.js") %>%
     html_text() %>%
-    str_extract(pattern = "Version [0-9].[0-9].[0-9], [A-Z][a-z]+ [0-9]:[a-z]+, [0-9]+")
+    str_extract(pattern = "Version [0-9].[0-9].[0-9], [A-Z][a-z]+ [0-9], [0-9]+")
 }
